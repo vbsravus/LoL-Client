54a8027391c583e53fc9830fb31b8a5997b6d786
Raí Raul <rair2544@gmail.com>
2023-01-10 15:10:21 -0300

    v0.1110012023

--- src/content/loginscreen/loginscreen.js
+++ src/content/loginscreen/loginscreen.js
@@ -3,17 +3,38 @@ import "./loginscreen.css"
 
 export default function Loginscreen () 
 {
+
+    const [checkStateMusic, setCheckStateMusic] = useState(false)
+    const [checkStateAnim, setCheckStateAnim] = useState(false)
+
+    useEffect(() => {
+        if(checkStateMusic === true){
+            document.querySelector('.videoSeasonIntro').volume = 0
+        }else{
+            document.querySelector('.videoSeasonIntro').volume = 1
+        }
+    }, [checkStateMusic])
+
+    useEffect(() => {
+        if(checkStateAnim === true){
+            document.querySelector('.StaticImage').style.display = "block"
+        }else{
+            document.querySelector('.StaticImage').style.display = "none"
+        }
+    }, [checkStateAnim])
+
     return(
         <div className="divContainerLoginScreen">
             <div className="divMainBodyLoginScreen">
 
                 {/* Parte do Video */}
                 <div className="divVideoBodyLoginScreen">
-                    <img src={require('./assets/images/RiotGamesLogo.PNG')}/>
-                    <video loop autoPlay={true} src={require('./assets/videos/seasonintro.mp4')} type="video/mp4"></video>
+                    <img className="StaticImage" src={require('./assets/images/StaticImage.png')}/>
+                    <img className="RiotLogo" src={require('./assets/images/RiotGamesLogo.PNG')}/>
+                    <video className="videoSeasonIntro" loop autoPlay={true} src={require('./assets/videos/seasonintro.mp4')} type="video/mp4"></video>
                     <div>
-                        <label><input type="checkbox"></input>Disable animation</label>
-                        <label><input type="checkbox"></input>Disable Music</label>
+                        <label><input onClick={() => setCheckStateAnim(!checkStateAnim)} checked={checkStateAnim} type="checkbox"></input>Disable animation</label>
+                        <label><input onClick={() => setCheckStateMusic(!checkStateMusic)} checked={checkStateMusic} type="checkbox"></input>Disable Music</label>
                     </div>
                 </div>
 
@@ -24,8 +45,8 @@ export default function Loginscreen ()
                     <input type="text" placeholder="USERNAME"></input>
                     <input type="password" placeholder="PASSWORD"></input>
                     <label><input type="checkbox"></input>Stay signed in</label>
-                    <button disabled="true">SIGN IN</button>
-                    <p>v0.1</p>
+                    <button disabled>SIGN IN</button>
+                    <p>v0.1110012023</p>
                 </div>
 
             </div>
